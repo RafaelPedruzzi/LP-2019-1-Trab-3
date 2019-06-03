84f64e2d4fb99613184d25aa2d0e0c2c92f8482b
Rafael Pedruzzi <rafaelbelmockpedruzzi@hotmail.com>
2019-06-03 11:20:52 -0300

    Printing saida.txt

--- LP_TRABALHO_3_Rafael_Belmock_Pedruzzi/main.hs
+++ LP_TRABALHO_3_Rafael_Belmock_Pedruzzi/main.hs
@@ -12,26 +12,23 @@ import Numeric
 
 import TrabPoint
 
--- Funções para conversão dos números lidos como String em Float:
--- Converte uma String para Float:
-leiaFloat :: String -> Float
-leiaFloat = read
--- Converte uma lista de Strings para Float:
-converteSF xs = map leiaFloat xs
+-- Funções para conversão dos números lidos como String em Double:
+-- Converte uma String para Double:
+leiaDouble :: String -> Double
+leiaDouble = read
+-- Converte uma lista de Strings para Double:
+converteSF xs = map leiaDouble xs
 
 -- Função que garante a impressão de 4 casas decimais:
-show4Decimals :: Float -> String
+show4Decimals :: Double -> String
 show4Decimals x = showFFloat (Just 4) x ""
 
 -- Função main. Ponto de partida do programa:
-main = do entrada <- readFile "entrada.txt" -- Abrindo arquivo entrada.txt
-          dist <- readFile "distancia.txt" -- Abrindo arquivo distancia.txt
-          putStrLn $ show $ addIndice $ map converteSF $ map words $ lines entrada
-          putStrLn $ show $ montaGrupos (leiaFloat dist) $ addIndice $ map converteSF $ map words $ lines entrada
-          --putStrLn $ show $ map show4Decimals $ map leiaFloat $ words entrada
-
--- Função que adiciona um índice ao inicio de cada ponto da listade pontos:
-addIndice xss = addIndiceAux xss 1
-
-addIndiceAux [] i = []
-addIndiceAux (xs:xss) i = (i : xs) : addIndiceAux xss (i+1)
+main = do
+    entrada <- readFile "entrada.txt" -- Abrindo arquivo entrada.txt
+    dist <- readFile "distancia.txt" -- Abrindo arquivo distancia.txt
+    --   putStrLn $ show $ montaPontos $ map converteSF $ map words $ lines entrada
+    --   putStrLn $ show $ montaGrupos (leiaDouble dist) $ montaPontos $ map converteSF $ map words $ lines entrada
+    let gss = montaGrupos (leiaDouble dist) $ montaPontos $ map converteSF $ map words $ lines entrada
+    writeFile "saida.txt" $ gsString gss
+    --putStrLn $ show $ map show4Decimals $ map leiaDouble $ words entrada
--- LP_TRABALHO_3_Rafael_Belmock_Pedruzzi/point.hs
+++ LP_TRABALHO_3_Rafael_Belmock_Pedruzzi/point.hs
@@ -7,15 +7,24 @@
 
 module TrabPoint where
 
+-- Estrutura que define o tipo ponto:
+data Ponto = Ponto {
+    i     :: Int,
+    coord :: [Double]
+} deriving Show
+
+-- Função que cria a lista de pontos:
+montaPontos xss = montaPontosAux xss 1
+
+montaPontosAux [] i = []
+montaPontosAux (xs:xss) i = Ponto{i = i,coord = xs} : montaPontosAux xss (i+1)
+
 -- Função que calcula a distância entre dois pontos:
--- Parâmetros: duas listas de Floats representando as cordenadas dos pontos.
+-- Parâmetros: dois pontos.
 -- Retorno: distancia euclidiana entre x1 e x2.
 -- Condições: os pontos devem ter o mesmo número de dimensões.
--- ATENÇÃO: a primeira posição de cada ponto é ignorada.
-distPoints (x1:xs1) (x2:xs2) = sqrt $ distPointsAux xs1 xs2
+distPoints (Ponto _ xs1) (Ponto _ xs2) = sqrt $ distPointsAux xs1 xs2
 
--- Função recursiva auxiliar de distPoints que realiza a soma do quadrado das diferenças de todas as coordenadas de x1 e x2:
--- Parâmetros: listas contendo as coordenadas restantes dos pontos.
 distPointsAux [] [] = 0
 distPointsAux (x1:xs1) (x2:xs2) = (x1 - x2) * (x1 - x2) + distPointsAux xs1 xs2
 
@@ -24,10 +33,18 @@ distPointsAux (x1:xs1) (x2:xs2) = (x1 - x2) * (x1 - x2) + distPointsAux xs1 xs2
 -- Parâmetros: a distância máxima entre um ponto e seu líder e a lista de pontos.
 -- Retorno: lista com os grupos formados (líder é o ponto na primeira posição da lista do grupo).
 -- Condição: todos os pontos devem ter o mesmo número de dimensões.
--- ATENÇÃO: a primeira posição de cada ponto é ignorada.
-montaGrupos :: Float -> [[Float]] -> [[[Float]]]
-
+montaGrupos :: Double -> [Ponto] -> [[Ponto]]
 montaGrupos dist [] = []
 montaGrupos dist (xs:xss) = grupo : montaGrupos dist [as | as <- xss, distPoints xs as > dist]
                     where
                         grupo = xs : [as | as <- xss, (distPoints xs as) <= dist]
+
+-- Funcão que cria a String para impressão no arquivo saida.txt
+-- parâmetros: a lista de grupos.
+gsString :: [[Ponto]] -> String
+gsString (g:[]) = gsStringAux g
+gsString (g:gs) = gsStringAux g ++ "\n\n" ++ gsString gs
+
+gsStringAux :: [Ponto] -> String
+gsStringAux ((Ponto i _):[]) = show i
+gsStringAux ((Ponto i _):ps) = (show i) ++ " " ++ gsStringAux ps
